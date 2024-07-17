d488af453090a7a8a099d1cd657c3b33011c897a
pranshu-bajaj <131748246+pranshu-bajaj@users.noreply.github.com>
2024-07-18 00:19:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+yolov_modelv3_weights = "./yolo-coco/yolov3.weights"
+yolov_modelv3_configuration = "./yolo-coco/yolov3.cfg"
+yolov_model = cv2.dnn.readNet(yolov_modelv3_weights, yolov_modelv3_configuration)
+
+coco_n = "./yolo-coco/coco.names"
+classes = []
+with open(coco_n,'r') as f:
+  classes = f.read().splitlines()
+
+
+def count(index):
+    img = cv2.imread(f"./Dataset-1/images/{index}.jpg")
+    height , width, _ = img.shape
+    plt.imshow(img)
+
+    # converting image to binary large object
+    blob = cv2.dnn.blobFromImage(img,1/255,(320,320),(0,0,0),swapRB=True,crop=False)
+
+    i= blob[0].reshape(320,320,3)
+    plt.imshow(i)
+
+    yolov_model.setInput(blob)
+
+    output_layers_name = yolov_model.getUnconnectedOutLayersNames()
+    layer_output = yolov_model.forward(output_layers_name)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layer_output:
+      for detection in output:
+        score = detection[5:]
+        class_id = np.argmax(score)
+        confidence = score[class_id]
+        if confidence > 0.7:
+          center_x = int(detection[0]*width)
+          center_y = int(detection[1]*height)
+          w = int(detection[2]*width)
+          h = int(detection[3]*height)
+
+          x = int(center_x-w/2)
+          y = int(center_y-h/2)
+
+          boxes.append([x,y,w,h])
+          confidences.append((float(confidence)))
+          class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes ,confidences , 0.5 , 0.4)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0,255,size=(len(boxes),3))
+
+    cnt_1 = 0
+    cnt_2 = 0
+    if len(indexes) > 0:
+      for i in indexes.flatten():
+        x,y,w,h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confi = str(round(confidences[i],3))
+        color = colors[i]
+        if(label == "bicycle" or label == "car" or label == "motorbike" or label == "bus" or label == "truck"):
+          if(x<width/2):
+            cnt_1 = cnt_1+1
+          else:
+            cnt_2 = cnt_2+1
+        cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
+        cv2.putText(img,label + " " + confi,(x,y+20),font,2,(1,1,1),2)
+
+    # can uncomment to see the image
+    cv2.imshow("image",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    l = [cnt_1,cnt_2]
+    return l
+
+
+
+def max_comb(vehicle_count,wait):
+    max_score=-1e9
+    mx_lanes=[]
+    check=[1,3,4]
+    for i in range(0,8,2):
+        for j in check:
+            score=vehicle_count[i]*wait[i]+vehicle_count[(i+j)%8]*wait[(i+j)%8]
+            if score>max_score:
+                max_score=score
+                mx_lanes=[i,(i+j)%8]
+    return mx_lanes
+
+
+
+
+cycles = 1
+n = 3 # no of cycles
+total_time = 0
+total_vehicles = 0
+
+while cycles <= n:
+    vehicle_count = []
+    for i in range(4):
+        a = random.randint(0,1200)
+        l = count(a) # computer vision
+        vehicle_count += l
+    print("\nCycle",cycles,"\n")
+    # vehicle_count = [3,2,5,5,2,3,5,1]
+    print("Count of vehicles in each lane:",vehicle_count)
+    wait = [8,8,8,8,8,8,8,8]
+
+    cnt = sum(vehicle_count) # total vehicles
+    vehicles = cnt
+    total_vehicles += vehicles
+    print("Total Vehicles:",cnt)
+    t = 0 # time
+    print("Initial time:",t,"\n")
+    cur_1,cur_2 = max_comb(vehicle_count,wait) # starting lanes
+    # assuming 1 sec for 1 vehicle
+    i = 0
+    while cnt > 0:
+        print("Current Lanes:",cur_1+1,cur_2+1)
+        c = min(vehicle_count[cur_1],vehicle_count[cur_2])
+        if c==0:
+            c = max(vehicle_count[cur_1],vehicle_count[cur_2])
+            cnt -= c
+            vehicle_count[cur_1] = 0
+            vehicle_count[cur_2] = 0
+        else:
+            vehicle_count[cur_1] -= c
+            vehicle_count[cur_2] -= c
+            cnt = cnt - 2*c
+        t += c
+        wait[cur_1] -= 1
+        wait[cur_2] -= 1
+        # updating current lanes
+        cur_1,cur_2 = max_comb(vehicle_count,wait)
+        print("Updated Time:",t)
+        print("Vehicles remaining:",cnt,"\n")
+
+    cycles += 1
+    total_time += t
+    print("Time taken for",vehicles,"vehicles:",t,"\n")
+
+print("\nNo of vehicles:",total_vehicles)
+print("Total time taken:",total_time,"\n")
